48c6c67b5c4a931f00216b60c709617a1e1978e7
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-05-12 12:54:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,6 +43,18 @@ sudo chown -R apache /var/www/html
 #wp theme install Consulting --allow-root
 #wp theme activate consulting --allow-root
 wp plugin install wordpress-importer --activate --allow-root
-
-sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
-sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDlvu4zNd+Ng4K5twKW3iaxvxXXD4pZ8iehQ8h+fDoDdEQIjV6pTfmTmFdYY1Ilt54ETvpvGSZkM3aPbqBX1HEmt3sc/JF8EjdQ63L0phhGnjulLeUIGCydNANZSedTfmcQ+llbaFIrhYNiKMOwkAARj8Sb3E1Y6ZsoUCGekPkDw8s1OlJEhIudxKT3Y7SsvCuP8aWgposC4DGBbBIIq+UipqBI0l6kOFB+fp8hPDY3x4AnrpxqeAgXKpTPYGP53z3vJF25l2K4s3+53mfy+c5c2NcoGxbE0hB1E5fWyaBun9vpRwtwGYBcmH+s0dDIWxR5P+5TsgJl7eO+kSLvDdPp jenkins@jenkins-cloud.novalocal" > /home/ec2-user/.ssh/authorized_keys
+git clone -b deploy --single-branch https://github.com/marylydiaj/zippyops_wordpress.git
+sudo cp /root/Wordpress/zippyopssite.wordpress.2020-04-22.000.xml /var/www/html/
+cd /var/www/html
+wp theme install Consulting --allow-root
+wp theme activate consulting --allow-root
+wp import zippyopssite.wordpress.2020-04-22.000.xml --authors=create --allow-root
+endpoint=`aws rds --region us-east-1 describe-db-instances --query "DBInstances[*].Endpoint.Address"`
+echo >file $endpoint
+sed -i 's/[][]//g' /root/file
+sed -i 's/"//g' /root/file
+sed -i 's/ //g' /root/file
+endpoint=$(<file)
+echo $endpoint
+mysql -u zippyops -pzippyops -h $endpoint --database zippyops < wordpressdb.sql
+systemctl restart httpd
